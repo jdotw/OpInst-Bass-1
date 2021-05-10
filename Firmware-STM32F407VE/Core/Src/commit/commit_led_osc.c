08874d95fb495049de3c64b99d5c45e4727a1d60
James Wilson <jw@jameswilson.io>
2021-05-10 20:48:27 +1000

    - fixed osc rgb LED error handling

--- Firmware-STM32F407VE/Core/Src/commit/commit_led_osc.c
+++ Firmware-STM32F407VE/Core/Src/commit/commit_led_osc.c
@@ -21,67 +21,67 @@ uint8_t _12_to_8(uint16_t a) {
 }
 
 void _commit_led_osc1() {
-	HAL_StatusTypeDef res;
+	bool res;
 
 	/* LEFT0:00 */
 
 	/* Osc1 Saw */
 	res = is32_set_rgb(I2C_LEFT, 0, 0, 0, 0xFF, 0x00, 0x00, _12_to_8(ctrl_value.osc1_saw_lvl));
-	if (res != HAL_OK) Error_Handler();
+	if (!res) Error_Handler();
 	res = is32_set_rgb(I2C_LEFT, 0, 0, 1, 0xFF, 0x00, 0x00, _12_to_8(ctrl_value.osc1_saw_lvl));
-	if (res != HAL_OK) Error_Handler();
+	if (!res) Error_Handler();
 
 	/* Osc1 Sq */
 	res = is32_set_rgb(I2C_LEFT, 0, 0, 2, 0x00, 0x00, 0xFF, _12_to_8(ctrl_value.osc1_squ_lvl));
-	if (res != HAL_OK) Error_Handler();
+	if (!res) Error_Handler();
 	res = is32_set_rgb(I2C_LEFT, 0, 0, 3, 0x00, 0x00, 0xFF, _12_to_8(ctrl_value.osc1_squ_lvl));
-	if (res != HAL_OK) Error_Handler();
+	if (!res) Error_Handler();
 
 	/* Osc1 Mix */
 	res = is32_set_rgb(I2C_LEFT, 0, 0, 4, _12_to_8(ctrl_value.osc1_saw_lvl), 0x00, _12_to_8(ctrl_value.osc1_squ_lvl), _max(ctrl_value.osc1_saw_lvl, ctrl_value.osc1_squ_lvl));
-	if (res != HAL_OK) Error_Handler();
+	if (!res) Error_Handler();
 
 	/* LEFT0:11 */
 
 	/* Osc2 Saw */
 	res = is32_set_rgb(I2C_LEFT, 0, 3, 0, 0xFF, 0x00, 0x00, _12_to_8(ctrl_value.osc2_saw_lvl));
-	if (res != HAL_OK) Error_Handler();
+	if (!res) Error_Handler();
 	res = is32_set_rgb(I2C_LEFT, 0, 3, 1, 0xFF, 0x00, 0x00, _12_to_8(ctrl_value.osc2_saw_lvl));
-	if (res != HAL_OK) Error_Handler();
+	if (!res) Error_Handler();
 
 	/* Osc2 Sq */
 	res = is32_set_rgb(I2C_LEFT, 0, 3, 2, 0x00, 0x00, 0xFF, _12_to_8(ctrl_value.osc2_squ_lvl));
-	if (res != HAL_OK) Error_Handler();
+	if (!res) Error_Handler();
 	res = is32_set_rgb(I2C_LEFT, 0, 3, 3, 0x00, 0x00, 0xFF, _12_to_8(ctrl_value.osc2_squ_lvl));
-	if (res != HAL_OK) Error_Handler();
+	if (!res) Error_Handler();
 
 	/* Osc2 Noise */
 	res = is32_set_rgb(I2C_LEFT, 0, 3, 2, 0x00, 0x00, 0xFF, _12_to_8(ctrl_value.osc2_noise_lvl));
-	if (res != HAL_OK) Error_Handler();
+	if (!res) Error_Handler();
 	res = is32_set_rgb(I2C_LEFT, 0, 3, 3, 0x00, 0x00, 0xFF, _12_to_8(ctrl_value.osc2_noise_lvl));
-	if (res != HAL_OK) Error_Handler();
+	if (!res) Error_Handler();
 
 	/* Osc2 Mix */
 	res = is32_set_rgb(I2C_LEFT, 0, 3, 4, _12_to_8(ctrl_value.osc1_saw_lvl), 0x00, _12_to_8(ctrl_value.osc1_squ_lvl), _max(ctrl_value.osc1_saw_lvl, ctrl_value.osc1_squ_lvl));
-	if (res != HAL_OK) Error_Handler();
+	if (!res) Error_Handler();
 
 	/* LEFT1:00 */
 
 	/* Sub */
 	res = is32_set_rgb(I2C_LEFT, 1, 0, 0, 0xFF, 0x00, 0x00, _12_to_8(ctrl_value.sub_lvl));
-	if (res != HAL_OK) Error_Handler();
+	if (!res) Error_Handler();
 	res = is32_set_rgb(I2C_LEFT, 1, 0, 1, 0xFF, 0x00, 0x00, _12_to_8(ctrl_value.sub_lvl));
-	if (res != HAL_OK) Error_Handler();
+	if (!res) Error_Handler();
 
 	/* Noise */
 	res = is32_set_rgb(I2C_LEFT, 1, 0, 2, 0x00, 0x00, 0xFF, _12_to_8(ctrl_value.sub_noise_lvl));
-	if (res != HAL_OK) Error_Handler();
+	if (!res) Error_Handler();
 	res = is32_set_rgb(I2C_LEFT, 1, 0, 3, 0x00, 0x00, 0xFF, _12_to_8(ctrl_value.sub_noise_lvl));
-	if (res != HAL_OK) Error_Handler();
+	if (!res) Error_Handler();
 
 	/* Sub Noise Mix */
 	res = is32_set_rgb(I2C_LEFT, 1, 0, 4, _12_to_8(ctrl_value.sub_lvl), 0x00, _12_to_8(ctrl_value.sub_noise_lvl), _max(ctrl_value.sub_lvl, ctrl_value.sub_noise_lvl));
-	if (res != HAL_OK) Error_Handler();
+	if (!res) Error_Handler();
 
 }
 
