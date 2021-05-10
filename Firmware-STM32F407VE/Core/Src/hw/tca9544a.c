f8fb12c2e5e62f00e8b9a2a44041803ba81dd565
James Wilson <jw@jameswilson.io>
2021-05-10 20:32:02 +1000

    - Fixed TCA9544A not selecting channel - Calibrates on boot

--- Firmware-STM32F407VE/Core/Src/hw/tca9544a.c
+++ Firmware-STM32F407VE/Core/Src/hw/tca9544a.c
@@ -9,7 +9,7 @@
 #include "main.h"
 #include "i2c.h"
 
-bool tca9544a_select (uint8_t bus, uint8_t channel, uint8_t mux_addr) {
+bool tca9544a_select (uint8_t bus, uint8_t mux_addr, uint8_t channel) {
 	uint8_t data = (channel & 0b11) | 0b100;
 	bool res = i2c_tx(bus, I2C_CHANNEL_DIRECT, mux_addr, &data, 1);
 	return res;
