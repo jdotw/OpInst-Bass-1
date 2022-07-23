873e107e9c1b6b157370e1d119fb1b3ed9baa302
James Wilson <jw@jameswilson.io>
2022-07-23 21:19:31 +1000

    Actually boots: - disable BluNRG sample app

--- Firmware-STM32F407VE/BlueNRG_2/App/app_bluenrg_2.c
+++ Firmware-STM32F407VE/BlueNRG_2/App/app_bluenrg_2.c
@@ -128,15 +128,15 @@ void MX_BlueNRG_2_Init(void)
 
   hci_init(APP_UserEvtRx, NULL);
 
-  PRINT_DBG("BlueNRG-2 BLE Sample Application\r\n");
+//  PRINT_DBG("BlueNRG-2 BLE Sample Application\r\n");
 
   /* Init Sample App Device */
-  ret = SampleAppInit();
-  if (ret != BLE_STATUS_SUCCESS)
-  {
-    PRINT_DBG("SampleAppInit()--> Failed 0x%02x\r\n", ret);
-    while(1);
-  }
+//  ret = SampleAppInit();
+//  if (ret != BLE_STATUS_SUCCESS)
+//  {
+//    PRINT_DBG("SampleAppInit()--> Failed 0x%02x\r\n", ret);
+//    while(1);
+//  }
 
   PRINT_DBG("BLE Stack Initialized & Device Configured\r\n");
 
