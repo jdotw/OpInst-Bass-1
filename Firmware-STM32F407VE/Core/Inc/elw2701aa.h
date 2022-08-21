993fa5d0481e43e09f4da4d9e6e2a7dfbbe9aeca
James Wilson <jw@jameswilson.io>
2022-08-21 10:15:17 +1000

    - adopted DMA for SPI - removed test data - adding in wait for bus to be ready

--- Firmware-STM32F407VE/Core/Inc/elw2701aa.h
+++ Firmware-STM32F407VE/Core/Inc/elw2701aa.h
@@ -12,7 +12,6 @@
 #include <stdbool.h>
 
 void elw2701aa_init(SPI_HandleTypeDef *hspi);
-void elw2701aa_test(SPI_HandleTypeDef *hspi, bool loop);
 void elw2701aa_write_data(SPI_HandleTypeDef *hspi, uint8_t start_x,
                           uint8_t x_len, uint8_t start_y, uint8_t y_len,
                           uint8_t (*data_callback)(uint16_t));
