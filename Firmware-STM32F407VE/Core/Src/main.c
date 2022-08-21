5b314d297d785b48ee9587ed9d6b8254f9411195
James Wilson <jw@jameswilson.io>
2022-08-21 10:14:45 +1000

    - remove oled commit from main loop - adjust time prescaler

--- Firmware-STM32F407VE/Core/Src/main.c
+++ Firmware-STM32F407VE/Core/Src/main.c
@@ -242,9 +242,6 @@ int main(void) {
     /* USER CODE END WHILE */
     // MX_BlueNRG_2_Process();
 
-    // Render OLED Display
-    oled_commit();
-
     /* USER CODE BEGIN 3 */
 
     // NOTE: Never let MX_BlueNRG_2_Process(); be called here
@@ -448,7 +445,7 @@ static void MX_TIM7_Init(void) {
   htim7.Instance = TIM7;
   htim7.Init.Prescaler = 2;
   htim7.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim7.Init.Period = 32000;
+  htim7.Init.Period = 50000;
   htim7.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_Base_Init(&htim7) != HAL_OK) {
     Error_Handler();
