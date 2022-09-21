1e3956292e603c8ec863a7bf6114956833ed5f63
James Wilson <jw@jameswilson.io>
2022-09-22 07:59:03 +1000

    added param_value() function

---
+++ Firmware-STM32F407VE/Core/Inc/param.h
@@ -0,0 +1,15 @@
+/*
+ * param.h
+ *
+ *  Created on: Sep 17, 2022
+ *      Author: jwilson
+ */
+
+#ifndef INC_PARAM_H_
+#define INC_PARAM_H_
+
+#include "ctrl.h"
+
+uint16_t param_value(ctrl_enum_t i);
+
+#endif /* INC_PARAM_H_ */
---
+++ Firmware-STM32F407VE/Core/Src/param/param.c
@@ -0,0 +1,19 @@
+/*
+ * param.c
+ *
+ *  Created on: 18 Sept 2022
+ *      Author: jwilson
+ */
+
+#include "param.h"
+#include "ctrl.h"
+#include "seq.h"
+
+uint16_t param_value(ctrl_enum_t i) {
+  ctrl_t *ctrl = ctrl_get_active();
+  ctrl_t *step_ctrl = seq_get_active_step_ctrl(seq_get());
+  if (step_ctrl && step_ctrl->changed[i])
+    return step_ctrl->value[i];
+  else
+    return ctrl->value[i];
+}
